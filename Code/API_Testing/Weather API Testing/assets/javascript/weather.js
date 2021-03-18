19af87678d1e48cee3d90093b7c5b9235aa2a4e1
drew-hockstein <andrew.hockstein@colorado.edu>
2021-03-18 08:00:03 -0600

    Added basic 7 day forecast functionality

--- Code/API_Testing/Weather API Testing/assets/javascript/weather.js
+++ Code/API_Testing/Weather API Testing/assets/javascript/weather.js
@@ -8,6 +8,20 @@ function getCondObj(response) {
     condObj['Feels Like'] = (response.current.feels_like);
     condObj['Temp-unit'] = 'F';
     condObj['Wind Speed'] = (response.current.wind_speed + ' mph');
+    condObj['Day 1 Max'] = (response.daily[1].temp.max);
+    condObj['Day 1 Min'] = (response.daily[1].temp.min);
+    condObj['Day 2 Max'] = (response.daily[2].temp.max);
+    condObj['Day 2 Min'] = (response.daily[2].temp.min);
+    condObj['Day 3 Max'] = (response.daily[3].temp.max);
+    condObj['Day 3 Min'] = (response.daily[3].temp.min);
+    condObj['Day 4 Max'] = (response.daily[4].temp.max);
+    condObj['Day 4 Min'] = (response.daily[4].temp.min);
+    condObj['Day 5 Max'] = (response.daily[5].temp.max);
+    condObj['Day 5 Min'] = (response.daily[5].temp.min);
+    condObj['Day 6 Max'] = (response.daily[6].temp.max);
+    condObj['Day 6 Min'] = (response.daily[6].temp.min);
+    condObj['Day 7 Max'] = (response.daily[7].temp.max);
+    condObj['Day 7 Min'] = (response.daily[7].temp.min);
 
     return condObj;
 }
@@ -21,6 +35,34 @@ function updateCards(condObj) {
     $('#degree-unit').text(condObj['Temp-unit']);
     $('#feels-like').text(Math.floor(condObj['Temperature']));
     $('#feels-like-degree-unit').text(condObj['Temp-unit']);
+    $('#1-max-degrees').text(Math.floor(condObj['Day 1 Max']));
+    $('#1-max-degree-unit').text(condObj['Temp-unit']);
+    $('#1-min-degrees').text(Math.floor(condObj['Day 1 Min']));
+    $('#1-min-degree-unit').text(condObj['Temp-unit']);
+    $('#2-max-degrees').text(Math.floor(condObj['Day 2 Max']));
+    $('#2-max-degree-unit').text(condObj['Temp-unit']);
+    $('#2-min-degrees').text(Math.floor(condObj['Day 2 Min']));
+    $('#2-min-degree-unit').text(condObj['Temp-unit']);
+    $('#3-max-degrees').text(Math.floor(condObj['Day 3 Max']));
+    $('#3-max-degree-unit').text(condObj['Temp-unit']);
+    $('#3-min-degrees').text(Math.floor(condObj['Day 3 Min']));
+    $('#3-min-degree-unit').text(condObj['Temp-unit']);
+    $('#4-max-degrees').text(Math.floor(condObj['Day 4 Max']));
+    $('#4-max-degree-unit').text(condObj['Temp-unit']);
+    $('#4-min-degrees').text(Math.floor(condObj['Day 4 Min']));
+    $('#4-min-degree-unit').text(condObj['Temp-unit']);
+    $('#5-max-degrees').text(Math.floor(condObj['Day 5 Max']));
+    $('#5-max-degree-unit').text(condObj['Temp-unit']);
+    $('#5-min-degrees').text(Math.floor(condObj['Day 5 Min']));
+    $('#5-min-degree-unit').text(condObj['Temp-unit']);
+    $('#6-max-degrees').text(Math.floor(condObj['Day 6 Max']));
+    $('#6-max-degree-unit').text(condObj['Temp-unit']);
+    $('#6-min-degrees').text(Math.floor(condObj['Day 6 Min']));
+    $('#6-min-degree-unit').text(condObj['Temp-unit']);
+    $('#7-max-degrees').text(Math.floor(condObj['Day 7 Max']));
+    $('#7-max-degree-unit').text(condObj['Temp-unit']);
+    $('#7-min-degrees').text(Math.floor(condObj['Day 7 Min']));
+    $('#7-min-degree-unit').text(condObj['Temp-unit']);
 }
 
 
