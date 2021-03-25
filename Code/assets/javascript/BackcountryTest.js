9f3be2c89c8ba4258d2ba6be78bba8becc45082f
drew-hockstein <andrew.hockstein@colorado.edu>
2021-03-25 04:54:25 -0600

    Connected the backcountry conditions page to the weather and maps api based on user inputted longitude and latitude

--- Code/assets/javascript/BackcountryTest.js
+++ Code/assets/javascript/BackcountryTest.js
@@ -3,6 +3,10 @@
     getWeather function. */
 $(document).ready(() => {
 
+    //loading map to world-view initially
+    $('#map').attr('src', `https://www.google.com/maps/embed/v1/view?key=${MAPS_KEY}
+    &center=39.5501,-105.7821&zoom=1&maptype=satellite`);
+
     console.log('Backcountry Page');
 
     //when submit button is clicked
@@ -12,7 +16,34 @@ $(document).ready(() => {
         var lat = document.getElementById("latitude").value;
         document.getElementById("lonText").innerHTML = lon;
         document.getElementById("latText").innerHTML = lat;
-
+        getBackcountryWeather(lat,lon)
     })
+
+    function getBackcountryWeather(resortLat, resortLon) {
+
+
+        let units = 'imperial';
+
+
+        let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${resortLat}&lon=${resortLon}&APPID=${WEATHER_KEY}&units=${units}`;
+
+
+        $.ajax({
+            url: weatherUrl,
+            method: 'GET'
+        }).then((response) => {
+            console.log('Api has been called.');
+
+            $('#map').attr('src', `https://www.google.com/maps/embed/v1/view?key=${MAPS_KEY}&center=${resortLat},${resortLon}&zoom=14&maptype=satellite`);
+
+            //weatherCode = response.weather[0].id;
+            //console.log(weatherCode);
+            console.log(response);
+
+            // Create and display the card showing weather conditions.
+            //updateCards(getCondObj(response));
+
+        })
+    }
 
 })
