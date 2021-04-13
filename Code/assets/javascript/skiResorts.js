5e51f038a8226a1da446e6e3f917a0da01d35a4b
drew-hockstein <andrew.hockstein@colorado.edu>
2021-04-13 03:02:06 -0600

    Added ski resorts to the resort conditions page

--- Code/assets/javascript/skiResorts.js
+++ Code/assets/javascript/skiResorts.js
@@ -23,5 +23,45 @@ let skiResorts = {
         "lat": "38.9000",
         "long": "-106.9659",
         "tz": "America/Denver"
+    },
+    "silverton-mountain": {
+        "lat": "37.8846",
+        "long": "-107.6659",
+        "tz": "America/Denver"
+    },
+    "steamboat-springs": {
+        "lat": "40.4850",
+        "long": "-106.8317",
+        "tz": "America/Denver"
+    },
+    "wolf-creek": {
+        "lat": "37.4722",
+        "long": "-106.7930",
+        "tz": "America/Denver"
+    },
+    "eldora": {
+        "lat": "39.9372",
+        "long": "-105.5827",
+        "tz": "America/Denver"
+    },
+    "copper": {
+        "lat": "39.5022",
+        "long": "-106.1497",
+        "tz": "America/Denver"
+    },
+    "arapahoe-basin": {
+        "lat": "39.6425",
+        "long": "-105.8719",
+        "tz": "America/Denver"
+    },
+    "aspen-highlands": {
+        "lat": "39.1824",
+        "long": "-106.8554",
+        "tz": "America/Denver"
+    },
+    "winter-park": {
+        "lat": "39.8868",
+        "long": "-105.7625",
+        "tz": "America/Denver"
     }
-}
+};
